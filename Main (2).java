5540d4ac67fa90e3b446f59c365cc82d213a4639
lucasxavila <164033498+lucasxavila@users.noreply.github.com>
2024-05-28 17:51:52 -0300

    Update Main (2).java

--- Main (2).java
+++ Main (2).java
@@ -1,25 +1,23 @@
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.IOException;
+import java.io.FileWriter;
 
 public class Main {
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
-
-    public static void main(String[] args) {
-        int numerador = 10;
-        int denominador = 0; // Isso causará uma exceção de divisão por zero
-        int resultado = 0;
-
-        try {
-            // Tenta realizar a divisão
-            resultado = numerador / denominador;
-            System.out.println("Resultado: " + resultado);
-            logger.log(Level.INFO, "Divisão realizada com sucesso: " + resultado);
-        } catch (ArithmeticException e) {
-            // Trata exceções aritméticas, como divisão por zero
-            logger.log(Level.SEVERE, "Erro ao realizar a divisão: " + e.getMessage(), e);
-        } finally {
-            // Este bloco será executado independentemente de ocorrer uma exceção ou não
-            logger.log(Level.INFO, "Bloco finally executado.");
-        }
+  public static void main(String[] args) throws IOException {
+    FileWriter writer = null;
+    writer = new FileWriter("log.txt");
+    int numerador = 10;
+    int denominador = 0;
+    int resultado = 0;
+    
+    try {
+      resultado = numerador / denominador;
     }
+    catch (ArithmeticException e) {
+      writer.write("Erro (divisão por 0): " + e.getMessage());
+    }
+    finally {
+      System.out.println("Operação finalizada");
+    }
+    writer.close();
+  }
 }
